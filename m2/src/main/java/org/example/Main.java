324c893707dd6b5093ab32736b19d73adfe0bc5b
Semenov Volodymyr <semenov7vs@gmail.com>
2023-04-30 21:39:41 +0300

    Create SumCalculatorTest, add Tests and Exception

--- m2/src/main/java/org/example/Main.java
+++ m2/src/main/java/org/example/Main.java
@@ -2,6 +2,20 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        SumCalculator sumCalculator = new SumCalculator();
+        System.out.println(sumCalculator.sum(3));
     }
+}
+class SumCalculator{
+    public int sum(int n){
+        if (n <= 0){
+            throw new IllegalArgumentException("Number isn't correct!");
+        }
+        int res = 0;
+        for (int i = 1; i <= n; i++) {
+            res+=i;
+        }
+        return res;
+    }
+
 }
